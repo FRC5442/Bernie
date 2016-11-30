cd4e8a5874a417437d625071d21925c0f9cc5f24
Joyfun8 <jcenglish02@wacohi.net>
2016-11-29 19:22:31 -0600

    Add autonomous program to move forward

---
+++ src/org/usfirst/frc0/MyRobot/commands/AutoEnglish.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc0.MyRobot.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutoEnglish extends CommandGroup {
+	
+	
+	
+	public AutoEnglish (){
+// Drive forward 1.5 rot at 65% speed
+	addequential
+}
+
+}
