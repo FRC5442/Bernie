8bc2fc4ab586c9a9026dced1909380a6fdade3c2
SeanHowarter <SBHowarter@wacohi.net>
2016-12-05 15:51:40 -0600

    Fixing Everything?

--- src/org/usfirst/frc0/MyRobot/commands/AutoFord.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc0.MyRobot.commands;
-
-import edu.wpi.first.wpilibj.command.CommandGroup;
-
-public class AutoFord extends CommandGroup {
-	public AutoFord(){
-		addSequential(new Drive_Command(12, 1));
-	}
-}
